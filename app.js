b511fb0aea5c91cc6f3a01dd612235eead76c9fa
Amol Magdum <amol.magdum@rank.com>
2025-02-18 23:25:50 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello to amol'));
+app.get('/', (req, res) => res.send('Hello to amol_added this to test pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
